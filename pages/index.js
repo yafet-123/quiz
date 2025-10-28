66175a466f2c31aac0b0641dc3d81d91ecb591d7
yafet-123 <yafetaddisu123@gmail.com>
2025-10-28 22:20:24 +0300

    edited the dashboard

--- pages/index.js
+++ pages/index.js
@@ -159,14 +159,14 @@ export default function Home() {
         <section className="py-20 bg-[#faf5ff] text-center text-black px-6">
           <FaEnvelopeOpenText className="text-5xl mx-auto mb-4" />
           <h3 className="text-3xl font-bold mb-4">Stay Updated</h3>
-          <p className="text-lg mb-8 max-w-xl mx-auto text-purple-100">
+          <p className="text-lg mb-8 max-w-xl mx-auto text-black">
             Get free tips, study resources, and updates straight to your inbox.
           </p>
           <form className="flex flex-col sm:flex-row justify-center gap-4 max-w-xl mx-auto">
             <input
               type="email"
               placeholder="Enter your email"
-              className="px-4 py-3 rounded-lg w-full sm:w-2/3 text-gray-800"
+              className="px-4 py-3 rounded-lg w-full sm:w-2/3 text-black0"
             />
             <button className="bg-[#9333ea] text-purple-700 font-semibold px-6 py-3 rounded-lg hover:bg-purple-100">
               Subscribe
