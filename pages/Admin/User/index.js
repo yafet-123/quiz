447226cbe83a6179afe89be17672c9899cb23b8f
yafet-123 <yafetaddisu123@gmail.com>
2025-11-05 20:24:40 +0300

    edited the article

--- pages/Admin/User/index.js
+++ pages/Admin/User/index.js
@@ -12,14 +12,14 @@ export async function getServerSideProps(context){
   const session = await getSession(context);
   const serverdate = new Date();     
   const userRole = session?.user?.role;
-  if (userRole !== 'admin') {
-    return {
-      redirect: {
-        destination: '/auth/Admin/Login/signin-user',
-        permanent: false,
-      },
-    };
-  }
+  // if (userRole !== 'admin') {
+  //   return {
+  //     redirect: {
+  //       destination: '/auth/Admin/Login/signin-user',
+  //       permanent: false,
+  //     },
+  //   };
+  // }
   
   const users = await prisma.User.findMany({orderBy : {modifiedAt:'desc'}});
   const Allusers = users.map((data)=>({
