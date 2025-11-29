0641987b11a8719192c75ccdc7c24beccf47a136
Yafet Addisu <yafetaddisu123@gmail.com>
2025-11-29 00:47:51 -0800

    edited the quiz part

--- pages/index.js
+++ pages/index.js
@@ -162,19 +162,19 @@ export default function Home() {
           <h3 className="text-3xl font-bold text-center text-[#417094] mb-12">
             Join Our Community
           </h3>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 text-center">
+          <div className="grid grid-cols-1 gap-10 text-center">
             <JoinCard
               icon={<FaUserGraduate />}
               title="Students"
               description="Get instant access to quizzes, notes, and progress tracking tools to help you study smarter."
               buttonText="Join as Student"
             />
-            <JoinCard
+            {/* <JoinCard
               icon={<FaChalkboardTeacher />}
               title="Teachers"
               description="Upload resources, create study materials, and help students achieve exam success."
               buttonText="Join as Teacher"
-            />
+            /> */}
           </div>
         </section>
 
